d469c0d4935920336fd14869f43786a469bd49c9
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-14 00:05:08 +0400

    Checked Pattern Matching

--- CSharp12Features/CSharp12Features/Program.cs
+++ CSharp12Features/CSharp12Features/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Reflection;
+﻿using CSharp12Features;
+using System.Reflection;
 
-WriteLine(Env.MachineName);
+//WriteLine(Env.MachineName);
 
 Assembly mainAssembly = Assembly.GetEntryAssembly()!;
 
@@ -15,12 +16,12 @@ foreach (AssemblyName name in mainAssembly.GetReferencedAssemblies())
         methodCount += type.GetMethods().Length;
     }
 
-    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", assembly.GetTypes().Length, methodCount, name.Name);
+    //WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", assembly.GetTypes().Length, methodCount, name.Name);
 }
 
 OutputEncoding = System.Text.Encoding.UTF8;
 string grinningEmoji = char.ConvertFromUtf32(0x1F600);
-WriteLine(grinningEmoji);
+//WriteLine(grinningEmoji);
 
 var person = new { FirstName = "Alice", Age = 56 };
 
@@ -32,14 +33,16 @@ string json = $$"""
  }
  """;
 
-WriteLine(json);
-WriteLine();
+//WriteLine(json);
+//WriteLine();
 
 unsafe
 {
-    WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
-    WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers in the range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
+    //WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
+    //WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers in the range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
 }
 
-dynamic something = new object();
-something.size();
+//dynamic something = new object();
+//something.size();
+
+Test.PatternMatching();
---
+++ CSharp12Features/CSharp12Features/Test.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp12Features;
+
+public static class Test
+{
+    public static void PatternMatching(int number = 5)
+    {
+        switch (number)
+        {
+            case 1:
+                WriteLine("One");
+                break; // Jumps to end of switch statement.
+            case 2:
+                WriteLine("Two");
+                goto case 1;
+            case 3: // Multiple case section.
+            case 4:
+                WriteLine("Three or four");
+                goto case 1;
+            case 5:
+                goto A_label;
+            default:
+                WriteLine("Default");
+                break;
+        } // End of switch statement.
+        WriteLine("After end of switch");
+        A_label:
+            WriteLine($"After A_label");
+    }
+}
