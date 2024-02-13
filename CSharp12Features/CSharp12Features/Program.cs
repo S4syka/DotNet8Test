61c2eb82365ef3df12359631e4694afa6e2e9f52
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-13 19:26:07 +0400

    Added System.Console statically

--- CSharp12Features/CSharp12Features/Program.cs
+++ CSharp12Features/CSharp12Features/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
-Console.WriteLine(Env.MachineName);
+WriteLine(Env.MachineName);
 
 Assembly mainAssembly = Assembly.GetEntryAssembly()!;
 
@@ -16,12 +15,12 @@ foreach (AssemblyName name in mainAssembly.GetReferencedAssemblies())
         methodCount += type.GetMethods().Length;
     }
 
-    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", assembly.GetTypes().Length, methodCount, name.Name);
+    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", assembly.GetTypes().Length, methodCount, name.Name);
 }
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+OutputEncoding = System.Text.Encoding.UTF8;
 string grinningEmoji = char.ConvertFromUtf32(0x1F600);
-Console.WriteLine(grinningEmoji);
+WriteLine(grinningEmoji);
 
 var person = new { FirstName = "Alice", Age = 56 };
 
@@ -33,13 +32,13 @@ string json = $$"""
  }
  """;
 
-Console.WriteLine(json);
-Console.WriteLine();
+WriteLine(json);
+WriteLine();
 
 unsafe
 {
-    Console.WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
-    Console.WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers in the range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
+    WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
+    WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers in the range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
 }
 
 dynamic something = new object();
