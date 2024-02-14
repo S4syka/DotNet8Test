6a386e238ab503ee3c2c42e3bf339cbf214ec979
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-14 17:54:44 +0400

    Tested record with keyword "with"

---
+++ CSharp12Features/ParameterTest/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterTest;
+
+public record Person
+{
+    private int _oe = 12;
+
+    public int Age { get; set; }
+    public string? Name { get; set; }
+
+    public Person(int age, string name)
+    {
+        Age = age;
+        Name = name;
+    }
+
+    public int ChangeOe()
+    {
+        return ++_oe;
+    }
+}
--- CSharp12Features/ParameterTest/Program.cs
+++ CSharp12Features/ParameterTest/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ParameterTest;
+
+Person person1 = new (12, "Giorgi");
+Person person2 = person1;
+
+person1.Name = "kote";
+Console.WriteLine(person2);
+Console.WriteLine(person1);
+
+person1 = person1 with { Name = "koswia" };
+Console.WriteLine(person2);
+Console.WriteLine(person1);
