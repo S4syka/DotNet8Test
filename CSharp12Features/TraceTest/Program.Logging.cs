6cac88764571899ce449a55e314fbcfed0a45d7b
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-14 11:50:03 +0400

    Implemented SourceDetailLogging

--- CSharp12Features/TraceTest/Program.Logging.cs
+++ CSharp12Features/TraceTest/Program.Logging.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 partial class Program
 {
@@ -8,7 +10,7 @@ partial class Program
         string settingsFile = Path.Combine("Settings", "TraceSettings.json");
         string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), settingsFile);
 
-        ConfigurationBuilder builder = new ();
+        ConfigurationBuilder builder = new();
         builder.AddJsonFile(settingsPath);
 
         IConfigurationRoot config = builder.Build();
@@ -19,5 +21,10 @@ partial class Program
         Debug.WriteLineIf(ts.TraceWarning, "Trace warning");
         Debug.WriteLineIf(ts.TraceInfo, "Trace information");
         Debug.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+    }
+
+    static void LogSourceDetails(bool condition, [CallerMemberName] string member = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+    {
+        Debug.WriteLineIf(condition, string.Format("[{0}]\n {1} on line {2}", filePath, member, lineNumber));
     }
 }
--- CSharp12Features/TraceTest/Program.cs
+++ CSharp12Features/TraceTest/Program.cs
@@ -1,3 +1,4 @@
 ﻿SetupLogging();
 
+LogSourceDetails(true);
 Console.ReadKey();
