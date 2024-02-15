e20a4a68dbf4165ca5a3d06c80b0f753875e42e1
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-15 17:49:54 +0400

    Tested new keyword in polymorphism

---
+++ CSharp12Features/PolymorhismTest/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorhismTest;
+
+internal class Person (string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+
+    public int Grows() => ++Age;
+}
+
+
+internal class UkugmartiPerson(string name, int age) : Person (name, age)
+{
+    public new int Grows() => --Age;
+}
--- CSharp12Features/PolymorhismTest/Program.cs
+++ CSharp12Features/PolymorhismTest/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using PolymorhismTest;
+
+Person givi = new("Givi", 12);
+Console.WriteLine(givi.Grows());
+
+UkugmartiPerson goga = new UkugmartiPerson("Goga", 12);
+Console.WriteLine(goga.Grows());
+givi = goga;
+Console.WriteLine(givi.Grows());
+
+
