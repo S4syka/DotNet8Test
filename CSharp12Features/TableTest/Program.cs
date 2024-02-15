2e988aa62786acf4ee7e7ddcfd9ab341a002f616
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-15 19:53:33 +0400

    Tested Spectre.Console

---
+++ CSharp12Features/TableTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+
+Table table = new Table();
+
+table.AddColumn("[blue]vax[/]");
+table.AddColumn("[green]chemi[/]");
+table.AddEmptyRow();
+table.AddEmptyRow();
+table.AddEmptyRow();
+table.AddEmptyRow();
+table.AddRow("[blue]es ramamagari[/]", "rame yofila");
+//table.AddRow("pizdec?", "pizdec?", "pizdec?");
+table.AddRow("pizdec?");
+
+AnsiConsole.Write(table);
