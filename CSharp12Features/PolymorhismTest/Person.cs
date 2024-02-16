85bb2179edb69481996f8ce838814b2b5280cff3
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-16 17:24:32 +0400

    Formatted code

--- CSharp12Features/PolymorhismTest/Person.cs
+++ CSharp12Features/PolymorhismTest/Person.cs
@@ -9,11 +9,11 @@ namespace PolymorhismTest;
 internal class Person (string name, int age)
 {
     public string Name { get; set; } = name;
+
     public int Age { get; set; } = age;
 
     public int Grows() => ++Age;
 }
-
 
 internal class UkugmartiPerson(string name, int age) : Person (name, age)
 {
