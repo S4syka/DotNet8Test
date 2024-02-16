455d3728abc06f6da7df5557988082f1e2938a69
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-16 15:14:31 +0400

    Added entities for model

---
+++ WorkingWithEFCore/Domain/Entities/Developer.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Developer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal? Salary { get; set; }
+    public Organization? Organization { get; set; }
+}
---
+++ WorkingWithEFCore/Domain/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+internal class Project
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Organization? Organization { get; set; }
+    public List<Project>? Developer { get; set; }
+}
