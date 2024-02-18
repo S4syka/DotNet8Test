3ca47b18f7f7544e289eccb095a2dd01b9297a0c
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-18 18:33:24 +0400

    Added Email attribute to developer

--- WorkingWithEFCore/Domain/Entities/Developer.cs
+++ WorkingWithEFCore/Domain/Entities/Developer.cs
@@ -4,6 +4,7 @@ public class Developer
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    public string? Email { get; set; }
     public decimal? Salary { get; set; }
     public Organization? Organization { get; set; }
 }
