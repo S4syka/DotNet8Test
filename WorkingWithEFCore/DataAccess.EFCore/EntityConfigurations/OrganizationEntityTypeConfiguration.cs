f941c69affe2e252f4446bbefc0e88472a2769d5
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-18 19:33:38 +0400

    Added modifications on Organizations

--- WorkingWithEFCore/DataAccess.EFCore/EntityConfigurations/OrganizationEntityTypeConfiguration.cs
+++ WorkingWithEFCore/DataAccess.EFCore/EntityConfigurations/OrganizationEntityTypeConfiguration.cs
@@ -9,7 +9,9 @@ internal sealed class OrganizationEntityTypeConfiguration : IEntityTypeConfigura
     public void Configure(EntityTypeBuilder<Organization> builder)
     {
         AddComments(builder);
+        AddDataRequirements(builder);
         AddColumnNames(builder);
+        AddColumnOrders(builder);
     }
 
     private void AddComments(EntityTypeBuilder<Organization> builder)
@@ -21,5 +23,24 @@ internal sealed class OrganizationEntityTypeConfiguration : IEntityTypeConfigura
     {
         builder.Property(o => o.Id)
             .HasColumnName("OrganizationId");
+    }
+
+    private void AddDataRequirements(EntityTypeBuilder<Organization> builder)
+    {
+        builder.Property(o => o.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+    }
+
+    private void AddColumnOrders(EntityTypeBuilder<Organization> builder)
+    {
+        builder.Property(o => o.Id)
+            .HasColumnOrder(0);
+
+        builder.Property(o => o.Description)
+            .HasColumnOrder(1);
+
+        builder.Property(o => o.Name)
+            .HasColumnOrder(2);
     }
 }
