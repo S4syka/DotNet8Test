cc349273d2aca29f2c1620f4f9bc56b4ea0026a3
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-21 17:26:31 +0400

    Implemented UnitOfWork

---
+++ AdminPanel/DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Repositories;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private Lazy<FoodRepository> _lazyFoodRepository = new Lazy<FoodRepository>();
+    private Lazy<GalleryRepository> _lazyGalleryRepository = new Lazy<GalleryRepository>();
+    private Lazy<HomePictureRepository> _lazyHomePictureRepository = new Lazy<HomePictureRepository>();
+    private Lazy<RoomRepository> _lazyRoomRepository = new Lazy<RoomRepository>();
+
+    public IFoodRepository FoodRepo  => _lazyFoodRepository.Value;
+    public IGalleryRepository GalleryRepo => _lazyGalleryRepository.Value;
+    public IHomePictureRepository HomePictureRepo => _lazyHomePictureRepository.Value;
+    public IRoomRepository RoomRepository => _lazyRoomRepository.Value;
+}
