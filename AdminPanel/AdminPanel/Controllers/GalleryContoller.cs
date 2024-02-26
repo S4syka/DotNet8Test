57548225aa922cc1bbe7aa7e9af38da398f43df4
Eke_bichi <ereklekhomasuridze@gmail.com>
2024-02-27 01:46:20 +0400

    Added GalleryController and views

---
+++ AdminPanel/AdminPanel/Controllers/GalleryContoller.cs
@@ -0,0 +1,52 @@
+﻿using Domain.DTOs;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers;
+
+public class GalleryController : Controller
+{
+    private readonly IGalleryRepository _galleryRepository;
+
+    public GalleryController(IUnitOfWork unitOfWork)
+    {
+        _galleryRepository = unitOfWork.GalleryRepo();
+    }
+
+    public ActionResult Index()
+    {
+        var galleries = _galleryRepository.Load();
+        return View(galleries);
+    }
+
+    public ActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Add(Gallery gallery)
+    {
+        _galleryRepository.Create(gallery);
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(Guid id)
+    {
+        var gallery = _galleryRepository.Read(id);
+        return View(gallery);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Gallery gallery)
+    {
+        _galleryRepository.Update(gallery);
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(Guid id)
+    {
+        _galleryRepository.Delete(id);
+        return RedirectToAction("Index");
+    }
+}
